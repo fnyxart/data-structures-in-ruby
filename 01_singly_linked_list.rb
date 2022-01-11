578eae0171b8a3c50185ab1f272cebe6cd20e734
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2022-01-11 10:02:58 +0000

    Add remove node method and test

--- 01_singly_linked_list.rb
+++ 01_singly_linked_list.rb
@@ -15,18 +15,6 @@ class SinglyLinkedList
     @length = 0
   end
 
-  # Since the list keeps pointers to its head and its tail, the complexity of this method is O(1)
-  def insert(data)
-    # Create a new node to insert the value into the list
-    node = Node.new(data)
-    # If head is nil - set the node as head, otherwise append this node after the tail
-    !@head ? @head = node : @tail.next = node
-    # Set tail to be equal with node
-    @tail = node
-    # Increment the length counter of the list
-    @length += 1
-  end
-
   # Method to display the linked list and its nodes, the complexity is O(n)
   def display
     puts "Singly linked list length = #{@length}"
@@ -40,12 +28,52 @@ class SinglyLinkedList
     # Mark the end of the linked list
     print "X (nil)\n"
   end
+
+  # Since the list keeps pointers to its head and its tail, the complexity of this method is O(1)
+  def insert(data)
+    # Create a new node to insert the value into the list
+    node = Node.new(data)
+    # If head is nil - set the node as head, otherwise append this node after the tail
+    !@head ? @head = node : @tail.next = node
+    # Set tail to be equal with node
+    @tail = node
+    # Increment the length counter of the list
+    @length += 1
+  end
+
+  # Since we may have to traverse the whole list to remove the node, the complexity of this method is O(n)
+  # Method to remove given node from the linked list and adjust pointers to keep the elements together
+  def remove(node)
+    # If no node with given data exists in the list, return nil
+    return nil unless node
+
+    # If node is head of the list
+    if node == @head
+      @head = if @head.next.nil?
+                #-- set head and tail to nil if head is the only node in the list
+                @tail = nil
+              else
+                #-- otherwise set the next node to be head
+                @head.next
+              end
+    # If node is not head of the list
+    else
+      # Set the pre_node variable to be the head
+      pre_node = @head
+      # Loop through the list while there is a pre_node and pre_node.next and they are not the node
+      pre_node = pre_node.next while pre_node && pre_node.next != node
+      # (Breaks with pre_node = preceding node of the node to be removed)
+      # Set the next pointer of pre_node to the next pointer of the node to be removed
+      pre_node.next = node.next if pre_node
+    end
+    # Reduce the length counter of the list
+    @length -= 1
+  end
 end
 
 ## Node; used in a singly linked list class
 class Node
-  attr_reader :data
-  attr_accessor :next
+  attr_accessor :data, :next
 
   def initialize(data)
     @data = data
--- 01_test.rb
+++ 01_test.rb
@@ -14,6 +14,8 @@ def testcase_insert
   test_list.insert(3)
   # Inserting a new node with data = 24
   test_list.insert(24)
+  # Inserting a new node with data = 12
+  test_list.insert(12)
 
   # Displaying the singly test list with inserted data
   test_list.display
@@ -21,10 +23,32 @@ def testcase_insert
 
   # Raise exceptions for insert function
   raise unless test_list.head.data == 3
-  raise unless test_list.head.next == test_list.tail
-  raise unless test_list.tail.data == 24
+  raise unless test_list.head.next.next == test_list.tail
+  raise unless test_list.tail.data == 12
   raise unless test_list.tail.next.nil?
-  raise unless test_list.length == 2
+  raise unless test_list.length == 3
+end
+
+
+# Testing the remove function of a singly linked list
+def testcase_remove
+  test_list = sgl_linked_list_init
+  test_list.insert(3)
+  test_list.insert(24)
+  test_list.insert(12)
+  # # Removing the node with data = 3 (head)
+  # test_list.remove(3)
+  # Removing the node with data = 12 (tail)
+  test_list.remove(24)
+  # # Removing a node that is not in the list = 1
+  # test_list.remove(1)
+
+  # Displaying the singly test list with removeed data
+  test_list.display
+  p test_list
+
+  # Raise exceptions for remove function
 end
 
 testcase_insert
+testcase_remove
